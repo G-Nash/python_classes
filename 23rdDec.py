0c00eb8d5f977dc60a38b2568ba7cd4ea0f025b6
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-23 12:30:33 +0530

    content

--- 23rdDec.py
+++ 23rdDec.py
@@ -3,9 +3,9 @@
 # print(7 & 9)
 
 
-print(5 >> 2)
+# print(5 >> 2)
 
-print(8 >> 2)
+# print(8 >> 2)
 
 
 # & and 
@@ -20,3 +20,31 @@ print(8 >> 2)
 
 
 #control statements
+#conditional statements --- if if else elif
+#loops --- for while
+# jump statements --- continue break pass
+
+
+
+# if 
+def givechocolate():
+    print("chocolate")
+
+def givenothing():
+    print("nothing")
+
+name=['one','two','three', 'four', 'five','nash', 'six', 'seven']
+for i in name:
+    if i!='nash':
+        givechocolate()
+        print(i)
+        print()
+    else:
+        print("-----------------")
+        givenothing()
+        print(i)
+        print("-----------------")
+
+
+# length=len(name)
+# print(length)
