bc3c97b8457fb7782bdd40cb62158df0b8fe1e10
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-24 12:22:09 +0530

    content

---
+++ 24th_dec.py
@@ -0,0 +1,29 @@
+# for i in range(10):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+# for i in range(10,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+
+
+# i=999999
+# while True:
+#     print("while..",i)
+#     i+=1
+
+
+i=0
+while i<=20:
+    if i%2==0:
+        print(i)
+    else:
+        pass
+    i+=1
+
+
