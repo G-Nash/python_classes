939040c7555e2c422be6270d71db8ddd6b9dcab5
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-24 12:42:46 +0530

    content

--- 24th_dec.py
+++ 24th_dec.py
@@ -18,12 +18,36 @@
 #     i+=1
 
 
-i=0
-while i<=20:
-    if i%2==0:
-        print(i)
-    else:
-        pass
-    i+=1
+# i=0
+# while i<=20:
+#     if i%2==0:
+#         print(i)
+#     else:
+#         pass
+#     i+=1
+
+
+for z in range(25):
+    spc=""
+    str=""
+    for i in range(25-z):
+        spc+=" "
+    for j in range(z+1):
+        str+="* "
+
+    print(spc, end=" ")
+    print(str)
+
+str1="* * * * *"
+for a in range(10):
+    spc1=""
+    for b in range(20):
+        spc1+=" "
+    
+    print(spc1, end=" ")
+    print(str1)
+
+
+
 
 
