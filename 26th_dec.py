c0372a9837d0b40de96935e17c56d054044aa1d1
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-26 12:58:53 +0530

    content

--- 26th_dec.py
+++ 26th_dec.py
@@ -6,16 +6,25 @@
 
 # nested loops 
 
-units=450
-if (units<=200):
-    if(units<=100):
-        cBill=units*(-5)
-    else:
-        cBill=0
-else:
-    if(units>200 and units<=350):
-        cBill=units*10
-    else:
-        cBill=units*20
+# units=450
+# if (units<=200):
+#     if(units<=100):
+#         cBill=units*(-5)
+#     else:
+#         cBill=0
+# else:
+#     if(units>200 and units<=350):
+#         cBill=units*10
+#     else:
+#         cBill=units*20
 
-print(f"{units} units consumed and current bill is {cBill}")
+# print(f"{units} units consumed and current bill is {cBill}")
+
+
+
+
+for i in range(1,11):
+    for j in range(1,31):
+        print("class",i, "roll no", j)
+        
+print("total students",i*j)        
