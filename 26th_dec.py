b958113e96605b43afad69033cd7ebbc2b7178e7
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-26 12:46:38 +0530

    content

--- 26th_dec.py
+++ 26th_dec.py
@@ -6,10 +6,16 @@
 
 # nested loops 
 
-units=400
-if (units<=350):
-    cBill=units*10
+units=450
+if (units<=200):
+    if(units<=100):
+        cBill=units*(-5)
+    else:
+        cBill=0
 else:
-    cBill=units*20
+    if(units>200 and units<350):
+        cBill=units*10
+    else:
+        cBill=units*20
 
-print(f"Units used is {units} and current bill is {cBill}")
+print(f"{units} units consumed and current bill is {cBill}")
