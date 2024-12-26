405037391e0bede1b33c46151f9fdf2d6f81f1bb
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-26 12:29:16 +0530

    content

---
+++ 26th_dec.py
@@ -0,0 +1,15 @@
+# break and continue
+
+# pass
+# is more like a placepholder
+# used to manage syntax error 
+
+# nested loops 
+
+units=400
+if (units<=350):
+    cBill=units*10
+else:
+    cBill=units*20
+
+print(f"Units used is {units} and current bill is {cBill}")
